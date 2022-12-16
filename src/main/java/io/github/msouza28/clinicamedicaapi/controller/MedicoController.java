c51700e53919d5e5fbfeecd63981b167fbfa0d3f
msouza28 <matheus.emiliano28@gmail.com>
2022-12-16 18:02:40 -0300

    Endpoint de dados do Medico

--- src/main/java/io/github/msouza28/clinicamedicaapi/controller/MedicoController.java
+++ src/main/java/io/github/msouza28/clinicamedicaapi/controller/MedicoController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import io.github.msouza28.clinicamedicaapi.medico.DadosAtualizacaoMedico;
 import io.github.msouza28.clinicamedicaapi.medico.DadosListaMedico;
 import io.github.msouza28.clinicamedicaapi.medico.DadosMedico;
+import io.github.msouza28.clinicamedicaapi.medico.DadosMedicoAtualizado;
 import io.github.msouza28.clinicamedicaapi.medico.Medico;
 import io.github.msouza28.clinicamedicaapi.medico.MedicoRepository;
 import jakarta.validation.Valid;
@@ -31,37 +34,58 @@ public class MedicoController {
 
 	@PostMapping
 	@Transactional
-	public void cadastrar(@RequestBody @Valid DadosMedico dados) {
-		repository.save(new Medico(dados));
+	public ResponseEntity<?> cadastrar(@RequestBody @Valid DadosMedico dados, UriComponentsBuilder uriBuilder) {
+
+		var medico = new Medico(dados);
+		repository.save(medico);
+
+		var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(new DadosMedicoAtualizado(medico));
 	}
 
 	@GetMapping
-	public Page<DadosListaMedico> listar(@PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
-		return repository.findAllByAtivoTrue(paginacao).map(DadosListaMedico::new);
+	public ResponseEntity<Page<DadosListaMedico>> listar(
+			@PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
+		var page = repository.findAllByAtivoTrue(paginacao).map(DadosListaMedico::new);
+		
+		return ResponseEntity.ok(page);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity detalhar(@PathVariable Long id) {
+
+		var medico = repository.getReferenceById(id);
+		
+		return ResponseEntity.ok(new DadosMedicoAtualizado(medico));
 	}
 
 	@PutMapping
 	@Transactional
-	public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+	public ResponseEntity<?> atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
 
 		var medico = repository.getReferenceById(dados.id());
 		medico.atualizarInformacoes(dados);
+
+		return ResponseEntity.ok(new DadosMedicoAtualizado(medico));
 	}
-	
-	//Exclusao total
+
+	// Exclusao logica
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> deletar(@PathVariable Long id) {
+		var medico = repository.getReferenceById(id);
+		medico.excluir();
+
+		return ResponseEntity.noContent().build();
+
+	}
+
+	// Exclusao total
 //	@DeleteMapping("/{id}")
 //	@Transactional
 //	public void deletar(@PathVariable Long id) {
 //		repository.deleteById(id);
 //	}
-	
-	//Exclusao logica
-	@DeleteMapping("/{id}")
-	@Transactional
-	public void deletar(@PathVariable Long id) {
-		var medico = repository.getReferenceById(id);
-		medico.excluir();
-		
-	}
 
 }
---
+++ src/main/java/io/github/msouza28/clinicamedicaapi/medico/DadosMedicoAtualizado.java
@@ -0,0 +1,10 @@
+package io.github.msouza28.clinicamedicaapi.medico;
+
+import io.github.msouza28.clinicamedicaapi.endereco.Endereco;
+
+public record DadosMedicoAtualizado(Long id, String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
+
+	public DadosMedicoAtualizado(Medico medico) {
+		this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
+	}
+}
